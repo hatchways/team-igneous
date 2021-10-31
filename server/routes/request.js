7c0a8c1be75b2ccb38aba4c52078b3db3f7fdec3
Nicholas Wiebe <digitalexistence18@gmail.com>
2021-10-31 17:48:45 -0600

    Change request route endpoint names

--- server/routes/request.js
+++ server/routes/request.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const { userRequests, createRequest, updateRequest } = require("../controllers/request");
 
-router.route("/requests").get(userRequests)
-router.route("/requests/new").post(createRequest)
-router.route("/requests/update").put(updateRequest);
+router.route("/all").get(userRequests)
+router.route("/new").post(createRequest)
+router.route("/update").put(updateRequest);
 
 module.exports = router;
