3673ee55bee5dd031957e89224df16ac4b247ceb
Nicholas Wiebe <digitalexistence18@gmail.com>
2021-10-31 17:44:26 -0600

    create API call for retrieving all a users requests

---
+++ client/src/helpers/APICalls/request.ts
@@ -0,0 +1,17 @@
+import { AuthApiData } from '../../interface/AuthApiData';
+import { FetchOptions } from '../../interface/FetchOptions';
+
+const request = async (): Promise<AuthApiData> => {
+  const fetchOptions: FetchOptions = {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' },
+    credentials: 'include',
+  };
+  return await fetch(`/request/all`, fetchOptions)
+    .then((res) => res.json())
+    .catch(() => ({
+      error: { message: 'Unable to connect to server. Please try again' },
+    }));
+};
+
+export default request;
