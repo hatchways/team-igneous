36d35e846147a60b28c2e83d064a71afdf81564e
Nicholas Wiebe <digitalexistence18@gmail.com>
2021-11-01 13:05:34 -0600

    created userRequests, createRequest, and updateRequest controllers

--- server/controllers/request.js
+++ server/controllers/request.js
@@ -1,47 +1,56 @@
+const request = require("../models/Request");
 
+//get all requests for "logged in user"
+const userRequests = async (req, res) => {
+  await request.find({user_id: req.params.user_id}, function (err, docs) {
+    if (err){
+        console.log("Error: ", err)
+        res.status(400).json(err);  
+      }
+    else{
+        console.log("Success: ", docs);
+        res.status(200).json(docs);  
+    }});
+};
 
-//get all requests for logged in user
-const userRequests = (req, res) => {
-  try {
-  const result = request.find();
-  return res.json(result);
-  } catch (err) {
-    console.log(err);
-    console.err(err);
-    message('failed to retrieve requests');
-    return res.json(message);
+//create a new request
+const createRequest = async (req, res) => {
+  const { user_id, sitter_id, start, end, accepted, declined, paid } = req.body.data;
+  await request.create({user_id:`${user_id}`, sitter_id:`${sitter_id}`, start:`${start}`, end:`${end}`, accepted:`${accepted}`, declined:`${declined}`, paid:`${paid}`}, function (err, docs) {
+    if (err){
+        console.log("Error: ", err)
+        res.status(400).json(err);  
+      }
+    else{
+        console.log("Created request: ", docs);
+        res.status(201).json(docs);  
+    }});
+};
+
+//update request if accepted or declined
+const updateRequest = async (req, res) => {
+  const { _id, answer, answerValue } = req.body.params;
+  if (answer === 'accepted') {
+    await request.findByIdAndUpdate({ _id }, { accepted: answerValue }, function (err, docs) {
+      if (err){
+          console.log("Error: ", err)
+          res.status(500).json(err);
+        }
+      else{
+          console.log("Updated request: ", docs);
+          res.status(200).json(docs);
+      }});
+  } else if (answer === 'declined') {
+    await request.findByIdAndUpdate({ _id }, { declined: answerValue }, function (err, docs) { 
+      if (err){
+      console.log("Error: ", err)
+      res.status(500).json(err); 
+    }
+      else{
+          console.log("Updated request: ", docs);
+          res.status(200).json(docs);
+      }});
   }
-}
+};
 
-
-//post a request
-const createRequest = (req, res) => {
-  try {
-  const result = request.create();
-  return res.json(result);
-  } catch (err) {
-    console.log(err);
-    console.err(err);
-    message('failed to retrieve requests');
-    return res.json(message);
-  }
-}
-
-
-//update all requests
-const updateRequest = (req, res) => {
-  try {
-  const query = req.body;
-  delete update[id]
-  return res.json(result);
-  } catch (err) {
-    console.log(err);
-    console.err(err);
-    message('failed to post a request at this time');
-    return res.json(message);
-  }
-}
-
-
-
-
+module.exports = { userRequests, createRequest, updateRequest };
