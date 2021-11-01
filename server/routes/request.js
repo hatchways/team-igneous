49ee20a46027bf6217f5bd624204cbb0b1f1059b
Nicholas Wiebe <digitalexistence18@gmail.com>
2021-11-01 13:28:12 -0600

    Create request route end-points, and update server/App.js to use them

--- server/routes/request.js
+++ server/routes/request.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const { userRequests, createRequest, updateRequest } = require("../controllers/request");
 
-router.route("/all").get(userRequests)
 router.route("/new").post(createRequest)
 router.route("/update").put(updateRequest);
+router.route("/all/:user_id").get(userRequests);
 
 module.exports = router;
