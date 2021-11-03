040f4d8577ab475351be0a47f9aa5c97fdbde111
Nicholas Wiebe <digitalexistence18@gmail.com>
2021-11-03 16:58:34 -0600

    Create styles for profile picture page

--- client/src/pages/ProfilePicture/useStyles.ts
+++ client/src/pages/ProfilePicture/useStyles.ts
@@ -1,5 +1,39 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles((theme) => ({}));
+const useStyles = makeStyles(() => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    padding: '8rem 10rem 2rem 5rem',
+  },
+  content: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    zIndex: -1,
+    overflow: 'hidden',
+  },
+  innerContentContainer: {
+    display: 'flex',
+  },
+  titleContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    color: 'black',
+    margin: '2rem',
+  },
+  profileImageContainer: {},
+  profileImage: {},
+  tipContainer: {},
+  tip: {},
+  uploadButtonContainer: {},
+  uploadButton: {},
+  deleteButtonContainer: {},
+  deleteButton: {},
+}));
 
 export default useStyles;
