457b7b50ec2fd52af27f6a8ed1fc120ab6d63d4e
Nicholas Wiebe <digitalexistence18@gmail.com>
2021-11-03 10:04:44 -0600

    Update route to use userId, and pass along extra information in the body

--- client/src/helpers/APICalls/newRequest.ts
+++ client/src/helpers/APICalls/newRequest.ts
@@ -2,22 +2,25 @@ import { AuthApiData } from '../../interface/AuthApiData';
 import { FetchOptions } from '../../interface/FetchOptions';
 
 const createNewRequest = async (
-  user_id: string,
-  sitter_id: string,
-  start: number,
-  end: number,
+  userName: string,
+  userId: string,
+  sitterName: string,
+  sitterId: string,
+  sitterProfile: string,
+  start: string,
+  end: string,
   accepted: boolean,
   declined: boolean,
   paid: boolean,
 ): Promise<AuthApiData> => {
-  const data = { user_id, sitter_id, start, end, accepted, declined, paid };
+  const data = { userName, userId, sitterName, sitterId, sitterProfile, start, end, accepted, declined, paid };
   const fetchOptions: FetchOptions = {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ data }),
     credentials: 'include',
   };
-  return await fetch(`/request/new`, fetchOptions)
+  return await fetch(`/${userId}/new`, fetchOptions)
     .then((res) => res.json())
     .catch((err) => ({
       error: { message: `Unable to connect to server. Please try again: ${err}` },
