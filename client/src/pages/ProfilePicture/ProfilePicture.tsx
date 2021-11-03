d81a1e61936dc8f9127cec6f7a74bf97666ad7cb
Nicholas Wiebe <digitalexistence18@gmail.com>
2021-11-03 16:57:53 -0600

    setup profile picture elements in MUI grid

--- client/src/pages/ProfilePicture/ProfilePicture.tsx
+++ client/src/pages/ProfilePicture/ProfilePicture.tsx
@@ -3,14 +3,47 @@ import AuthMenu from '../../components/AuthMenu/AuthMenu';
 import Grid from '@material-ui/core/Grid';
 import SideNav from '../../components/SideNav/SideNav';
 import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
 
 export default function ProfilePicture(): JSX.Element {
   const classes = useStyles();
   return (
     <Grid container component="main">
       <AuthMenu />
-      <SideNav />
-      <Typography component="h1">Profile Picture</Typography>
+      <Grid item xs={8} sm={8} md={8} lg={8} xl={8} container className={classes.content && classes.root}>
+        <SideNav />
+        <Grid
+          item
+          xs={7}
+          sm={7}
+          md={7}
+          lg={7}
+          xl={7}
+          container
+          direction="column"
+          component={Paper}
+          className={classes.innerContentContainer}
+        >
+          <Grid item container className={classes.titleContainer}>
+            <Typography className={classes.title} variant="h1">
+              Profile Picture
+            </Typography>
+          </Grid>
+          <Grid item container className={classes.profileImageContainer}>
+            <img src="" className={classes.profileImage}></img>
+          </Grid>
+          <Grid item container className={classes.tipContainer}>
+            <Typography variant="h6" className={classes.tip}>
+              Be sure to use a photo that<br></br>clearly shows your face
+            </Typography>
+          </Grid>
+          <Grid item container className={classes.uploadButtonContainer}></Grid>
+          <Button className={classes.uploadButton}>Upload a file from your device</Button>
+          <Grid item container className={classes.deleteButtonContainer}></Grid>
+          <Button className={classes.deleteButton}>delete photo</Button>
+        </Grid>
+      </Grid>
     </Grid>
   );
 }
