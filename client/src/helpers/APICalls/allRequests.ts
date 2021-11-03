1a330988e126d022a6c561082b17c39fe72b822b
Nicholas Wiebe <digitalexistence18@gmail.com>
2021-11-03 10:02:38 -0600

    Update APICall to use /requests route

--- client/src/helpers/APICalls/allRequests.ts
+++ client/src/helpers/APICalls/allRequests.ts
@@ -1,13 +1,14 @@
 import { AuthApiData } from '../../interface/AuthApiData';
 import { FetchOptions } from '../../interface/FetchOptions';
 
-const getAllRequests = async (user_id: string): Promise<AuthApiData> => {
+const getAllRequests = async (userId: string): Promise<AuthApiData> => {
+  console.log('USEROID', userId);
   const fetchOptions: FetchOptions = {
     method: 'GET',
     headers: { 'Content-Type': 'application/json' },
     credentials: 'include',
   };
-  return await fetch(`/request/all/${user_id}`, fetchOptions)
+  return await fetch(`/${userId}/requests`, fetchOptions)
     .then((res) => res.json())
     .catch((err) => ({
       error: { message: `Unable to connect to server. Please try again: ${err}` },
