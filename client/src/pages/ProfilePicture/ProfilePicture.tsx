1d4f1e0e1bbc8a9b875f88f711fe9f38e0831eb3
Nicholas Wiebe <digitalexistence18@gmail.com>
2021-11-03 22:49:11 -0600

    Finalize profile picture content layout

--- client/src/pages/ProfilePicture/ProfilePicture.tsx
+++ client/src/pages/ProfilePicture/ProfilePicture.tsx
@@ -5,45 +5,58 @@ import SideNav from '../../components/SideNav/SideNav';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import Box from '@material-ui/core/Box';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 export default function ProfilePicture(): JSX.Element {
   const classes = useStyles();
   return (
-    <Grid container component="main">
+    <Box className={classes.outsideContainer}>
       <AuthMenu />
-      <Grid item xs={8} sm={8} md={8} lg={8} xl={8} container className={classes.content && classes.root}>
+      <Grid container component="main" className={classes.root}>
         <SideNav />
-        <Grid
-          item
-          xs={7}
-          sm={7}
-          md={7}
-          lg={7}
-          xl={7}
-          container
-          direction="column"
-          component={Paper}
-          className={classes.innerContentContainer}
-        >
-          <Grid item container className={classes.titleContainer}>
-            <Typography className={classes.title} variant="h1">
-              Profile Picture
-            </Typography>
+        <Grid item xs={8} sm={8} md={8} lg={8} xl={8} container className={classes.content}>
+          <Grid
+            item
+            xs={12}
+            sm={12}
+            md={12}
+            lg={12}
+            xl={12}
+            container
+            direction="column"
+            component={Paper}
+            className={classes.innerContentContainer}
+          >
+            <Grid item container className={classes.titleContainer}>
+              <Typography className={classes.title} variant="h1">
+                Profile Picture
+              </Typography>
+            </Grid>
+            <Grid item container className={classes.profileImageContainer}>
+              <img src="" className={classes.profileImage}></img>
+            </Grid>
+            <Grid item container className={classes.tipContainer}>
+              <Typography variant="h6" className={classes.tip}>
+                <Box>Be sure to use a photo that</Box>
+                <Box>clearly shows your face</Box>
+              </Typography>
+            </Grid>
+            <Grid item container className={classes.uploadButtonContainer}>
+              <Button variant="text" className={classes.uploadButton}>
+                Upload a file from your device
+              </Button>
+            </Grid>
+            <Grid item container className={classes.deleteButtonContainer}>
+              <IconButton className={classes.deleteButton}>
+                <DeleteIcon className={classes.deleteButtonIcon} />
+                Delete photo
+              </IconButton>
+            </Grid>
           </Grid>
-          <Grid item container className={classes.profileImageContainer}>
-            <img src="" className={classes.profileImage}></img>
-          </Grid>
-          <Grid item container className={classes.tipContainer}>
-            <Typography variant="h6" className={classes.tip}>
-              Be sure to use a photo that<br></br>clearly shows your face
-            </Typography>
-          </Grid>
-          <Grid item container className={classes.uploadButtonContainer}></Grid>
-          <Button className={classes.uploadButton}>Upload a file from your device</Button>
-          <Grid item container className={classes.deleteButtonContainer}></Grid>
-          <Button className={classes.deleteButton}>delete photo</Button>
         </Grid>
       </Grid>
-    </Grid>
+    </Box>
   );
 }
