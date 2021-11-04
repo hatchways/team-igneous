3769d98b4ac1c543fe354bed641de8c419540779
Nicholas Wiebe <digitalexistence18@gmail.com>
2021-11-04 14:40:32 -0600

    Change Link to NavLink for the purpose of displaying the current Page Link in Black Color

--- client/src/components/SideNav/SideNav.tsx
+++ client/src/components/SideNav/SideNav.tsx
@@ -1,30 +1,30 @@
-import { Link } from 'react-router-dom';
 import Grid from '@material-ui/core/Grid';
 import useStyles from './useStyles';
 import Paper from '@material-ui/core/Paper';
+import { NavLink } from 'react-router-dom';
 
 export default function SideNav(): JSX.Element {
   const classes = useStyles();
   return (
     <Grid item xs={2} sm={2} md={2} lg={2} xl={2} container component={Paper} className={classes.sideBar}>
-      <Link to="/user/edit-profile" className={classes.link}>
+      <NavLink to="/user/edit-profile" className={classes.link}>
         Edit Profile
-      </Link>
-      <Link to="/user/profile-photo" className={classes.link}>
+      </NavLink>
+      <NavLink to="/user/profile-photo" className={classes.link}>
         Profile Photo
-      </Link>
-      <Link to="/user/image-gallery" className={classes.link}>
+      </NavLink>
+      <NavLink to="/user/image-gallery" className={classes.link}>
         Image Gallery
-      </Link>
-      <Link to="/user/payment" className={classes.link}>
+      </NavLink>
+      <NavLink to="/user/payment" className={classes.link}>
         Payment
-      </Link>
-      <Link to="/user/security" className={classes.link}>
+      </NavLink>
+      <NavLink to="/user/security" className={classes.link}>
         Security
-      </Link>
-      <Link to="/user/settings" className={classes.link}>
+      </NavLink>
+      <NavLink to="/user/settings" className={classes.link}>
         Settings
-      </Link>
+      </NavLink>
     </Grid>
   );
 }
