c7bf24e28000aa20186272f3af956c82c00219ec
Nicholas Wiebe <digitalexistence18@gmail.com>
2021-11-04 14:41:14 -0600

    Remove commented out code not in use

--- client/src/components/SideNav/useStyles.ts
+++ client/src/components/SideNav/useStyles.ts
@@ -2,7 +2,6 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
   sideBar: {
-    // position: 'relative',
     display: 'flex',
     justifyContent: 'flex-start',
     alignItems: 'center',
