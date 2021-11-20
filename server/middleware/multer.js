a48754dcc1a0d3affd2726350eca2820b1639bc5
Nicholas Wiebe <digitalexistence18@gmail.com>
2021-11-20 16:42:12 -0700

    Add a filename Option to the storage object

--- server/middleware/multer.js
+++ server/middleware/multer.js
@@ -1,20 +1,25 @@
 const multer = require("multer");
 
-const storage = multer.diskStorage({
-  destination: "./uploads",
-});
-
-const fileFilter = (req, file, cb) => {
-  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+const fileFilter = (req, files, cb) => {
+  if (files.mimetype === "image/jpeg" || files.mimetype === "image/png") {
     cb(null, true);
   } else {
     cb({ message: "unsupported file format" }, false);
   }
 };
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "--" + file.originalname);
+  },
+});
+
 const upload = multer({
-  storage: storage,
-  fileFilter: fileFilter,
+ storage: storage,
+ fileFilter: fileFilter
 });
 
 module.exports = upload;
