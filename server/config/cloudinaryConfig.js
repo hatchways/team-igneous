9f4e76fa470c1e0a16914faa29ac553cf0e3b539
Nicholas Wiebe <digitalexistence18@gmail.com>
2021-11-20 16:45:20 -0700

    Remove unnecessary variable

--- server/config/cloudinaryConfig.js
+++ server/config/cloudinaryConfig.js
@@ -2,8 +2,6 @@ const cloudinary = require("cloudinary");
 const dotenv = require("dotenv");
 dotenv.config();
 
-//Must Export this
-const cloudinaryConfig = () =>
   cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.API_KEY,
